547dc752540b2e5bf4b5e9de3b4bfe676c7a6fc1
r-sugi <r.sugimoto0615@gmail.com>
2020-07-01 03:12:07 +0900

    update: clean graphql query

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo'
 import client from './client'
-import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
-
-const ME = gql`
-  query me {
-    user(login: "r-sugi") {
-      name
-      avatarUrl
-    }
-  }
-`
+import { ME } from './graphql'
 
 function App() {
   return (
