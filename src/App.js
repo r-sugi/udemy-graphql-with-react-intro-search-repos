704b0fa6115fa4989357668a6a2563b32d28026d
r-sugi <r.sugimoto0615@gmail.com>
2020-07-03 01:18:15 +0900

    update: onClick addStar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider, Mutation, Query } from 'react-apollo'
 import client from './client'
-import { Query } from 'react-apollo'
-import { SEARCH_REPOSITORIES } from './graphql'
+import { ADD_STAR, SEARCH_REPOSITORIES } from './graphql'
 
 const StarButton = props => {
   const node = props.node
   const totalCount = node.stargazers.totalCount
   const viewerHasStarred = node.viewerHasStarred
   const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`
+  const StarStatus = ({ addStar }) => {
+    return (
+      <button
+        onClick={
+          () => {
+            addStar({
+              variables: { input: {starrableId: node.id } }
+            })
+          }
+        }
+      >
+        { starCount } | {viewerHasStarred ? `starred` : `-`}
+      </button>
+    )
+  }
+
   return (
-    <button>
-      { starCount } | {viewerHasStarred ? `starred` : `-`}
-    </button>
+    <Mutation mutation={ADD_STAR}>
+      {
+        addStar => <StarStatus addStar={addStar} />
+      }
+    </Mutation>
   )
 }
 
